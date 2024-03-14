a37d5d7358967c40a17d37da236afede908eca71
5huu <wahednarahil40@gmail.com>
2024-03-15 02:11:09 +0530

    Update password generator script to support words and numbers separately

--- passwordgen.py
+++ passwordgen.py
@@ -1,19 +1,19 @@
 import argparse
 from itertools import permutations
 
-def generate_passwords(words, min_length=1, max_length=None, separator=''):
+def generate_passwords(words, numbers, min_length=1, max_length=None, separator=''):
     passwords = []
     if max_length is None:
-        max_length = len(words)
+        max_length = len(words) + len(numbers)
 
     for r in range(min_length, max_length + 1):
-        for perm in permutations(words, r):
-            password = separator.join(perm)
+        for perm in permutations(words + numbers, r):
+            password = separator.join(map(str, perm))
             passwords.append(password)
 
     return passwords
 
-def write_to_file(passwords, filename='passwordlist.txt'):
+def write_to_file(passwords, filename='wordlist.txt'):
     with open(filename, 'w') as f:
         f.write('\n'.join(passwords))
     print(f"Word list generated and saved to {filename}")
@@ -25,13 +25,15 @@ if __name__ == "__main__":
     parser.add_argument('-max', type=int, default=4, help='Maximum length of passwords')
     parser.add_argument('-separator', type=str, default='', help='Separator between words in passwords')
     parser.add_argument('-w', '--words', nargs='+', help='List of words for password generation')
+    parser.add_argument('-n', '--numbers', nargs='+', type=int, help='List of numbers for password generation')
     args = parser.parse_args()
 
     min_length = args.min
     max_length = args.max
     separator = args.separator
-    words = args.words
+    words = args.words or []
+    numbers = args.numbers or []
 
-    passwords = generate_passwords(words, min_length, max_length, separator)
+    passwords = generate_passwords(words, numbers, min_length, max_length, separator)
     write_to_file(passwords)
 
